9be24e13ad4cf69a64ac6c01596231bf346687e9
ThirteenTears <CSmith15@csmith155.workstations.winona.edu>
2015-09-08 22:32:49 -0500

    Lab2 Upload
    Upload of Lab2

--- Lab2/src/Lab2a.java
+++ Lab2/src/Lab2a.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 public class Lab2a {
 	/*
@@ -6,35 +7,63 @@ public class Lab2a {
 	public static void main(String[] args) {
 		int intSmallest = 1000;
 		int intLargest = 0;
+		int int2small = 1000;
+		int int2large = 0;
 		int intLpos = 0;
 		int intSpos = 0;
+		int intLpos2 = 0;
+		int intSpos2 = 0;
+		int i = 0;
 		int[] intArray = new int[24];
 		Random generator = new Random();
 		
-		for(int i = 0; i < 24; i++)
+		for(i = 0; i < 24; i++)
 		{
-			intArray[i] = generator.nextInt(1000);
-			for(int j = 0; j < 24; j++)
-			{
-				if(intSmallest > intArray[i])
-				{
-					intSmallest = intArray[i];
-					intSpos = i;
-				}
-					
-				
-				if(intLargest < intArray[i])
-				{
-					intLargest = intArray[i];
-					intLpos = i;
-				}
-					
-			}
+			intArray[i] = generator.nextInt(1000);	
 		}
-		System.out.println("The smallest value in the array is: " + intSmallest);
-		System.out.println("The largest value in the array is: " + intLargest);
+		for(i = 0; i < 24; i++)
+		{
+			if(int2small > intArray[i] && intArray[i] > intSmallest)
+			{
+				int2small = intArray[i];
+				intSpos2 = i;
+			}
+			if(int2large < intArray[i] && intArray[i] < intLargest)
+			{
+				int2large = intArray[i];
+				intLpos2 = i;
+			}
+			if(intSmallest > intArray[i])
+			{
+				intSmallest = intArray[i];
+				intSpos = i;
+			}
+			if(intLargest < intArray[i])
+			{
+				intLargest = intArray[i];
+				intLpos = i;
+			}
+			
+		}
+		int j = 0;
+		int[] intArraySorted = new int[24];
+		for(i = 0; i < 24; i++)
+		{
+			while(intArray[i] <= intArraySorted[j]);
+			{
+				j++;
+			}
+			
+			
+		}
 		
 		
+		
+		System.out.println("The smallest value in the array is: " + intSmallest + ". [" + intSpos + "]");
+		System.out.println("The largest value in the array is: " + intLargest + ". [" + intLpos + "]");
+		System.out.println("The second smallest value in the array is: " + int2small + ". [" + intSpos2 + "]");
+		System.out.println("The second largest value in the array is: " + int2large + ". [" + intLpos2 + "]");
+		System.out.println("The median value in the array is: ");
 
 	}
 
