165486d4d87a3787f9c18ae4a1474f4b40c02440
ThirteenTears <caleb.dawson.smith@gmail.com>
2015-09-23 01:12:53 -0500

    Assignment One Start Over

--- Assignment One/src/Polynomial.java
+++ Assignment One/src/Polynomial.java
@@ -1,17 +1,77 @@
-import java.io.FileNotFoundException;
-
+import java.util.StringTokenizer;
 public class Polynomial {
-	public String polynomial = "";
-	OutputPoly outPoly = new OutputPoly();
-	
-	public String evalPoly(int i) throws FileNotFoundException
+	public String[] arrPoly = new String[21];
+	int counter = 0;
+	public Polynomial(String input)
 	{
 		
-		
-		
-		
-		return "test";
+		StringTokenizer stk = new StringTokenizer(input);
+		while(stk.hasMoreTokens() == true)
+		{
+			arrPoly[counter] = stk.nextToken();
+			counter++;
+		}
+	}
+	public String toString()
+	{
+		String strReturn = "";
+		strReturn = arrPoly[0] + "(";
+		for(int i = 1; i < 22; i++)
+		{
+			if(i % 2 == 0)
+			{
+				if(Integer.parseInt(arrPoly[i]) > 1 && Integer.parseInt(arrPoly[i +1]) > 0)
+				{
+					strReturn = strReturn.concat("^" + arrPoly[i] + " + ");
+				}
+				else if(Integer.parseInt(arrPoly[i]) > 1 && Integer.parseInt(arrPoly[i + 1]) < 0)
+				{
+					strReturn = strReturn.concat("^" + arrPoly[i] + " - ");
+				}
+				else if(Integer.parseInt(arrPoly[i]) == 1)
+				{
+					if(Integer.parseInt(arrPoly[i + 1]) > 0)
+					{
+						strReturn = strReturn.concat(" + ");
+					}
+					else if(Integer.parseInt(arrPoly[i + 1]) < 0)
+					{
+						strReturn = strReturn.concat(" - ");
+					}
+					else
+					{
+						strReturn = strReturn.concat(")");
+						break;
+					}
+				}
+				else if(Integer.parseInt(arrPoly[i]) == 0)
+				{
+					strReturn = strReturn.concat(")");
+					break;
+				}
+			}
+			if(i % 2 != 0)
+			{
+				if(Integer.parseInt(arrPoly[i + 1]) > 0)
+				{
+					if(arrPoly[i] == "1")
+					{
+						strReturn = strReturn.concat("X");
+					}
+					else
+					{
+						strReturn = strReturn.concat(Math.abs(Integer.parseInt(arrPoly[i])) + "X");
+					}
+				}
+				else
+				{
+					strReturn = strReturn.concat(Math.abs(Integer.parseInt(arrPoly[i])) + "");
+				}
+			}
+		}
+		return strReturn;
 	}
 	
 	
 }
+
